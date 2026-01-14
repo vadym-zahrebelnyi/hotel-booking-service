1042f16cb61ce4cf7a35b837dafc95dd1cab9742
Nikita <hkorobko@gmail.com>
2026-01-15 00:01:17 +0200

    Remove booking signals and restore default app configuration

--- booking/apps.py
+++ booking/apps.py
@@ -2,8 +2,4 @@ from django.apps import AppConfig
 
 
 class BookingConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "booking"
-
-    def ready(self):
-        import booking.signals
+    name = 'booking'
