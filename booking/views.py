a537917af0c25a8272222636bea415d5b8641380
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2026-01-14 21:39:49 +0200

    feat: restrict bookings visibility by user role

--- booking/views.py
+++ booking/views.py
@@ -12,3 +12,12 @@ class BookingViewSet(ReadOnlyModelViewSet):
     filter_backends = [DjangoFilterBackend]
     filterset_class = BookingFilter
 
+    def get_queryset(self):
+        queryset = Booking.objects.select_related("room", "user")
+
+        if self.request.user.is_staff:
+            return queryset
+
+        return queryset.filter(user=self.request.user)
+
+
