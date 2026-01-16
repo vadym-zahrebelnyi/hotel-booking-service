ab5d5c423cee52b3a6a1a4992c5cbcc96147d7a3
Nikita <hkorobko@gmail.com>
2026-01-16 23:14:41 +0200

    Implement telegram-notifications

--- booking/signals.py
+++ booking/signals.py
@@ -6,17 +6,33 @@ from notifications.tasks import send_telegram_notification
 
 
 @receiver(post_save, sender=Booking)
-def booking_created_notification(sender, instance, created, **kwargs):
-    if not created:
-        return
+def booking_notification(sender, instance, created, **kwargs):
+    if created:
+        message = (
+            "🆕 New booking created\n"
+            f"User: {instance.user.email}\n"
+            f"Room: {instance.room.number}\n"
+            f"Check-in: {instance.check_in_date}\n"
+            f"Check-out: {instance.check_out_date}\n"
+            f"Price per night: {instance.price_per_night}"
+        )
+        send_telegram_notification.delay(message)
 
-    message = (
-        "🆕 New booking created\n"
-        f"User: {instance.user.email}\n"
-        f"Room: {instance.room.number}\n"
-        f"Check-in: {instance.check_in_date}\n"
-        f"Check-out: {instance.check_out_date}\n"
-        f"Price per night: {instance.price_per_night}"
-    )
+    else:
+        if instance.status == "Cancelled":
+            message = (
+                "❌ Booking Canceled\n"
+                f"User: {instance.user.email}\n"
+                f"Room: {instance.room.number}\n"
+                f"Dates: {instance.check_in_date} - {instance.check_out_date}"
+            )
+            send_telegram_notification.delay(message)
 
-    send_telegram_notification.delay(message)
+        elif instance.status == "No show":
+            message = (
+                "⚠️ Guest No-show\n"
+                f"User: {instance.user.email}\n"
+                f"Room: {instance.room.number}\n"
+                "Description: Guest did not check in on time."
+            )
+            send_telegram_notification.delay(message)
