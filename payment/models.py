17de1e608affedf82600b0aa26c1930f9d277c4e
dmytro-malyk-dm <dmytro.malyk7@gmail.com>
2026-01-18 19:15:19 +0200

    add: field with creation date of payment

--- payment/models.py
+++ payment/models.py
@@ -24,3 +24,4 @@ class Payment(models.Model):
     session_url = models.URLField(max_length=500)
     session_id = models.CharField(max_length=255)
     money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
